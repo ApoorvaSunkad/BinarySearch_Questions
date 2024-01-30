777f23c05b8ea89b32e28c04717cef6ee9084fb8
ApoorvaSunkad@15 <apoorvasunkad1999@gmail.com>
2024-01-30 15:40:31 +0530

    Binary search STL

--- firstandlastoccurrencesofX.cpp
+++ firstandlastoccurrencesofX.cpp
@@ -82,3 +82,59 @@ while(start<=end){
 return {first,last};
 
 }
+
+
+//Using C++ STL functions of binary search
+
+#include<bits/stdc++.h>
+using namespace std;
+
+vector<int> find(int arr[], int n , int x ){
+    
+    //binary_search STL function takes 3 parameters that are
+    //The starting address of the array
+    //The ending address of the array(arr+n) tells that the index considered is the index
+    //after the last element
+    //an element to search
+
+    //Returns a boolean value depending upon if ele is present or not
+
+    if(!binary_search(arr,arr+n,x)){
+        return {-1,-1};
+    }
+    
+    int first = -1, last = -1;
+    
+    //These Lower bound and upper bound return the iterators 
+    //hence if we want indexes then we need to substract the
+    //starting address so that the index is returned.
+
+    //Lower bound function always gives the iterator which is 
+    //greater than equal to(>=) the element that we are searching.
+
+    //for ex: arr[] = {1,2,2,2,2,3,4}
+    //here 2 appears at 1st index and at 4th index in the above array
+    //So lower bound function's iterator will have address at start and address 
+    //after last element of array and now 2 is found
+    //so when we substract arr it gives the index as "1"
+
+    first = lower_bound(arr,arr+n,x)-arr;
+    last = upper_bound(arr,arr+n,x)-arr-1;
+
+    //Upper bound function always gives the iterator which is only
+    //greater than (>) the element that we are searching.
+
+    //for ex: arr[] = {1,2,2,2,2,3,4}
+    //here 2 appears at 4th index in the above array
+    //So upper bound function's iterator will have address at start and address 
+    //after last element of array. So here the iterator will point to the element 3 but we want 
+    //last occurrenc of 2 hence in the upper bound function we substract 1 from the iterator so that 
+    //we get the exact index of element 2 and now 2 is found
+    //so when we substract arr it gives the index as "4"
+    
+    return {first,last};
+}
+
+
+//For better understanding watch keerti purswani's video
+//Video Link: https://www.youtube.com/watch?v=SnrAclUvgPw&list=PLBsIjVjZHP8pfwgl9tb72lPadnZ_S8U_m&index=4
